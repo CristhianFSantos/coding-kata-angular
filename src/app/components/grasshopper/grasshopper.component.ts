2e5047894ca0839bbd0a2b6f76d66d97a1b69231
CristhianFSantos <cristhianfelipe0@gmail.com>
2021-09-17 01:25:53 -0300

    ✨ feat: Implemented function greet

--- src/app/components/grasshopper/grasshopper.component.ts
+++ src/app/components/grasshopper/grasshopper.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ck-grasshopper',
   templateUrl: './grasshopper.component.html',
 })
-export class GrasshopperComponent implements OnInit {
-  constructor() {}
+export class GrasshopperComponent {
+  message = '';
 
-  ngOnInit(): void {}
+  separateDataByComma(data: string): string[] {
+    return data.replace(' ', '').split(',');
+  }
+
+  greet(data: string) {
+    this.message = '';
+    const [name, owner] = this.separateDataByComma(data);
+
+    const messages = ['Hello boss', 'Hello guest'];
+
+    if (name != owner) {
+      this.message = messages[1];
+    } else {
+      this.message = messages[0];
+    }
+  }
 }
