d6a442cfe20d9344a5c3a5b52f338ffca7bb2bc4
CristhianFSantos <cristhianfelipe0@gmail.com>
2021-09-17 11:16:01 -0300

    📦 refactor: Change in function call logic

--- src/app/components/magic-function/magic-function.component.ts
+++ src/app/components/magic-function/magic-function.component.ts
@@ -7,16 +7,23 @@ import { Component } from '@angular/core';
 export class MagicFunctionComponent {
   message: any;
 
+  receiveAndProcessData(data: string): void {
+    const arrayData = data.replace(' ', '').split(',');
+
+    this.magicFunction(arrayData);
+  }
+
   magicFunction(data: any): void {
     const acceptedTypes = ['number', 'string', 'boolean'];
     let parametersJoin = '';
-    const countParameters = arguments.length;
+    const countParameters = data.length;
+
     for (let i = 0; i < countParameters; i++) {
-      if (!acceptedTypes.includes(typeof arguments[i])) {
+      if (!acceptedTypes.includes(typeof data[i])) {
         this.message = undefined;
         return;
       }
-      parametersJoin += `${arguments[i]}-`;
+      parametersJoin += `${data[i]}-`;
     }
     this.message = parametersJoin.substring(0, parametersJoin.length - 1);
   }
