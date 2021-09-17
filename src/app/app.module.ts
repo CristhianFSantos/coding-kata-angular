579b1f68e6dee4dfb3fca336705b1fc5ae4e0100
CristhianFSantos <cristhianfelipe0@gmail.com>
2021-09-17 00:59:50 -0300

    ✨ feat: grasshopper component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GrasshopperComponent } from './components/grasshopper/grasshopper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrasshopperComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/grasshopper/grasshopper.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ck-grasshopper',
+  templateUrl: './grasshopper.component.html',
+})
+export class GrasshopperComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
